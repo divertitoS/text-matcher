86fe35d6ebbfdd85086631cc63078f92867a0fe1
Stanislav Aleksandrovych <stanislav.aleksandrovych@firstgroup.com>
2024-09-09 21:37:07 +0300

    Initial commit

---
+++ src/main/java/com/bigid/textmatcher/TextMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.bigid.textmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextMatcherApplication.class, args);
+	}
+
+}
